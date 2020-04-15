be4dbcd6f5991a75e57e63a57b3cd2aac00341c3
Atsuo Ishimoto <atsuoishimoto@gmail.com>
2020-04-15 09:13:36 +0900

    heh

--- miyadaiku_theme_bootstrap4/__init__.py
+++ miyadaiku_theme_bootstrap4/__init__.py
@@ -1,5 +1,4 @@
-from miyadaiku.core.contents import bin_loader
-from miyadaiku.core import config
+import pkg_resources
 
 from . __version__ import __version__
 
@@ -17,16 +16,17 @@ def load_package(site):
     css = CSS_MIN if f else CSS
     css_path = 'externals/css/'+css
 
-    content = bin_loader.from_package(site, __name__, css_path, DEST_PATH+css)
-    site.contents.add(content)
+    content = pkg_resources.resource_string(__name__, css_path)
+    site.files.add_bytes("binary", DEST_PATH + css, content )
     site.config.add('/', {'bootstrap4_css_path': DEST_PATH+css})
 
 
     js = JS_MIN if f else JS
     js_path = 'externals/js/'+js
     
-    content = bin_loader.from_package(site, __name__, js_path, DEST_PATH+js)
-    site.contents.add(content)
+    content = pkg_resources.resource_string(__name__, js_path)
+    site.files.add_bytes("binary", DEST_PATH + js, content )
     site.config.add('/', {'bootstrap4_js_path': DEST_PATH+js})
 
-    site.add_template_module('bootstrap4', 'miyadaiku.themes.bootstrap4!macros.html')
+    site.add_template_module('bootstrap4', 'miyadaiku_theme_bootstrap4!macros.html')
+
--- setup.py
+++ setup.py
@@ -2,30 +2,29 @@ import os
 import re
 import pathlib
 from setuptools import setup, find_packages
-from miyadaiku.common import setuputils
+from miyadaiku import setuputils
 
 DIR = pathlib.Path(__file__).resolve().parent
-os.chdir(DIR)
 
 
 requires = [
     "miyadaiku",
-    "miyadaiku.themes.jquery",
-    "miyadaiku.themes.popper_js",
+    "miyadaiku_theme_jquery",
+    "miyadaiku_theme_popper_js",
 ]
 
-srcdir = 'node_modules/bootstrap/dist'
-destdir = 'miyadaiku/themes/bootstrap4/externals'
+srcdir = DIR / 'node_modules/bootstrap/dist'
+destdir = DIR / 'miyadaiku_theme_bootstrap4/externals'
 copy_files = [
-    [srcdir+'/css/', ['bootstrap.css', 'bootstrap.min.css', ], destdir+'/css/'],
-    [srcdir+'/js/', ['bootstrap.css', '*.js', ], destdir+'/js/']
+    [srcdir/ 'css/', ['bootstrap.css', 'bootstrap.min.css', ], destdir / 'css/'],
+    [srcdir / 'js/', ['bootstrap.css', '*.js', ], destdir / 'js/']
 ]
 
-versionpy = DIR / 'miyadaiku/themes/bootstrap4/__version__.py'
+versionpy = DIR / 'miyadaiku_theme_bootstrap4/__version__.py'
 version = re.search(r'"([\d.]+)"', versionpy.read_text())[1]
 
 setup(
-    name="miyadaiku.themes.bootstrap4",
+    name="miyadaiku_theme_bootstrap4",
     version=version,
     author="Atsuo Ishimoto",
     license="MIT",
@@ -36,12 +35,12 @@ setup(
     ],
     description='Bootstrap 4 files for miyadaiku static site generator',
     long_description=setuputils.read_file(DIR, 'README.rst'),
-    url='https://github.com/miyadaiku/miyadaiku-themes-bootstrap4',
+    url='https://github.com/miyadaiku/miyadaiku-theme-bootstrap4',
     project_urls={
         'Miyadaiku': 'https://miyadaiku.github.io/',
     },
 
-    packages=list(setuputils.list_packages(DIR, 'miyadaiku')),
+    packages=list(setuputils.list_packages(DIR, 'miyadaiku_theme_bootstrap4')),
     package_data={
         '': setuputils.SETUP_FILE_EXTS,
     },
