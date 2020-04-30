b1a353b08f05fcbd005fc849e51cc007b9c706c7
Atsuo Ishimoto <atsuoishimoto@gmail.com>
2020-04-30 18:23:27 +0900

    update setup

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-import pathlib
 from setuptools import setup
-from miyadaiku import setuputils
 
-DIR = pathlib.Path(__file__).resolve().parent
-
-srcdir = DIR / 'node_modules/bootstrap/dist'
-destdir = DIR / 'miyadaiku_theme_bootstrap4/externals'
-copy_files = [
-    [srcdir/ 'css/', ['bootstrap.css', 'bootstrap.min.css', ], destdir / 'css/'],
-    [srcdir / 'js/', ['bootstrap.css', '*.js', ], destdir / 'js/']
-]
-
-setup(
-    cmdclass={'copy_files': setuputils.copy_files},
-    copy_files=copy_files
-)
+setup()
